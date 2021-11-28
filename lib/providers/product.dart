8f6f755a67f86ad9dd2ca93e1d79e9ed75c8c08b
Andrew <yanovi4.a@gmail.com>
2021-11-28 16:39:26 +0100

    197

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,10 +1,12 @@
-class Product {
+import 'package:flutter/foundation.dart';
+
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
   final double price;
   final String imageUrl;
-  bool isfavorite;
+  bool isFavorite;
 
   Product({
     required this.id,
@@ -12,6 +14,11 @@ class Product {
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isfavorite = false,
+    this.isFavorite = false,
   });
+
+  void toggleFavoriteStatus(){
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,22 +1,25 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  const ProductItem({
-    Key? key,
-    required this.id,
-    required this.title,
-    required this.imageUrl,
-  }) : super(key: key);
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // const ProductItem({
+  //   Key? key,
+  //   required this.id,
+  //   required this.title,
+  //   required this.imageUrl,
+  // }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -24,23 +27,25 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routName,
-              arguments: id,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
+            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).colorScheme.secondary,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
