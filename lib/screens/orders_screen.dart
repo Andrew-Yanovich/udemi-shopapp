3bc4f48ea84e11ae8668f9f9436527064855fac5
Andrew <a.yanovich@platform.live>
2021-11-29 15:34:57 +0100

    211 - Added Drawer() with links

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 import '../providers/orders.dart' show Orders;
 
@@ -13,8 +14,9 @@ class OrdersScreen extends StatelessWidget {
     final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Orders'),
+        title: const Text('Your Orders'),
       ),
+      drawer: const AppDrawer(),
       body: ListView.builder(
         itemCount: ordersData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(order: ordersData.orders[i]),
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
 import './cart_screen.dart';
 import '../providers/cart.dart';
 import '../widgets/products_grid.dart';
@@ -63,6 +64,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: const AppDrawer(),
       body: ProductsGrid(showFavs: _showOnlyFavorites),
     );
   }
