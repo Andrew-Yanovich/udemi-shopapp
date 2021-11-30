0a22622d6e2c158a838883d1610c006d06976f8d
Andrew <a.yanovich@platform.live>
2021-11-30 15:22:26 +0100

    221 - added route for EditProductScreen. Added form field to EditProductScreen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
+  static const routName = '/edit-product';
+
   const EditProductScreen({Key? key}) : super(key: key);
 
   @override
@@ -8,13 +10,26 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Product'),
       ),
-      body: null,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
