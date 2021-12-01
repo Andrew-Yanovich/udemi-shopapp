4d8479d7a9c33a681aedc034010f597ac3038bd7
Andrew <yanovi4.a@gmail.com>
2021-12-01 16:25:10 +0100

    246 - error handling

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -46,8 +46,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     if (_isInit) {
       final productId = ModalRoute.of(context)!.settings.arguments;
       if (productId != null) {
-        _editedProduct =
-            Provider.of<Products>(context, listen: false).findById(productId.toString());
+        _editedProduct = Provider.of<Products>(context, listen: false)
+            .findById(productId.toString());
         _initValues = {
           'title': _editedProduct.title,
           'description': _editedProduct.description,
@@ -104,7 +104,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog(context: context, builder: (ctx) => AlertDialog(
+          title: const Text('An error occured!'),
+          content: const Text('Something went wrong!'),
+          actions: [
+            TextButton(onPressed: (){
+              Navigator.of(ctx).pop();
+            }, child: const Text('Okay'))
+          ],
+        ));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
