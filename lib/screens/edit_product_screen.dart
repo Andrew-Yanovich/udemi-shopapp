af107a20a2454575894e0636438005956a010134
Andrew <yanovi4.a@gmail.com>
2021-12-01 08:58:18 +0100

    added validation to title input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -47,7 +47,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
-    _form.currentState.save();
+    final isValid = _form.currentState!.validate();
+    if(!isValid){
+      return;
+    }
+    _form.currentState!.save();
   }
 
   @override
@@ -73,6 +77,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if(value!.isEmpty){
+                    return 'Please provide a value.';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
