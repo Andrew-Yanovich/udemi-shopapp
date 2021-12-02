0cbf5183b26a596f2bf3b97dfa00fa9d87ea0422
Andrew <yanovi4.a@gmail.com>
2021-12-02 15:13:34 +0100

    267 - added signIn functionality

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,12 +8,21 @@ class Auth with ChangeNotifier{
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signUp(String email, String password) async {
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCrDGa_JDwc1I0V6FNe82sBAwmzFur3nHc');
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCrDGa_JDwc1I0V6FNe82sBAwmzFur3nHc');
     final response = await http.post(url, body: json.encode({
       'email': email,
       'password': password,
       'returnSecureToken': true,
     }));
   }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+
 }
